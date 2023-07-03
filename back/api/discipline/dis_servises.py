7451896f2454603c50c31321681bfc76d472302e
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-07-03 13:32:22 +0500

    add func for update and delete

--- back/api/discipline/dis_servises.py
+++ back/api/discipline/dis_servises.py
@@ -8,6 +8,11 @@ from api.discipline.dis_dals import DisciplineDAL
 from data_base.db_models import Discipline
 
 
+from sqlalchemy.ext.asyncio import AsyncSession
+from data_base.session import get_db
+from fastapi import Depends
+
+
 async def _create_new_discipline(body: CreateDiscipline, session: AsyncSession) -> ShowDiscipline:
     async with session.begin():
         dis_dal = DisciplineDAL(session)
@@ -21,17 +26,31 @@ async def _create_new_discipline(body: CreateDiscipline, session: AsyncSession) 
         )
 
 
-async def _get_all_discipline(session: AsyncSession) -> List[Discipline] | None:
+async def _get_all_discipline(session: AsyncSession) -> List[ShowDiscipline] | None:
     async with session.begin():
         dis_dal = DisciplineDAL(session)
-        disciplines = await dis_dal.get_all_discipline()
-        if disciplines is not None:
-            return disciplines
+        discipline_list = await dis_dal.get_all_discipline()
+        if discipline_list is not None:
+            return [ShowDiscipline(dis_id=discipline.dis_id, name=discipline.name) for discipline in discipline_list]
 
 
-async def _delete_discipline(dis_id, session: AsyncSession) -> ShowDiscipline | None:
+async def _delete_discipline(dis_id: int, session: AsyncSession) -> ShowDiscipline | None:
     async with session.begin():
         dis_dal = DisciplineDAL(session)
-        discipline: ShowDiscipline = await dis_dal.delete_discipline(dis_id)
-        if discipline is not None:
-            return discipline
+        deleted_discipline = await dis_dal.delete_discipline(dis_id)
+        if deleted_discipline is not None:
+            return ShowDiscipline(
+                dis_id=deleted_discipline.dis_id,
+                name=deleted_discipline.name
+            )
+
+
+async def _update_discipline(dis_id: int, session: AsyncSession, **kwargs) -> ShowDiscipline | None:
+    async with session.begin():
+        dis_dal = DisciplineDAL(session)
+        updated_discipline = await dis_dal.update_discipline(dis_id, **kwargs)
+        if updated_discipline is not None:
+            return ShowDiscipline(
+                dis_id=updated_discipline.dis_id,
+                name=updated_discipline.name
+            )
