8f49213d8e9bffbfcf22dec0275608070a6482c3
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-07-03 13:31:22 +0500

    add delete and update methods

--- back/api/discipline/dis_dals.py
+++ back/api/discipline/dis_dals.py
@@ -17,12 +17,29 @@ class DisciplineDAL:
         await self.db_session.flush()
         return new_discipline
 
+    async def get_discipline_by_id(self, dis_id: int) -> Discipline | None:
+        return await self.db_session.get(Discipline, dis_id)
+
     async def get_all_discipline(self) -> List[Discipline] | None:
         query = select(Discipline)
         res = await self.db_session.execute(query)
         discipline_list = res.scalars().all()
         if discipline_list is not None:
             return discipline_list
+
+    async def delete_discipline(self, dis_id) -> Discipline | None:
+        query = select(Discipline).where(Discipline.dis_id == dis_id)
+
+        res = await self.db_session.execute(query)
+        discipline: Discipline | None = res.scalar_one_or_none()
+        if discipline:
+            try:
+                await self.db_session.delete(discipline)
+                await self.db_session.commit()
+            except Exception:
+                await self.db_session.rollback()
+
+        return discipline
 
     async def update_discipline(self, dis_id, **kwargs) -> Discipline | None:
         query = (
@@ -33,19 +50,6 @@ class DisciplineDAL:
         )
 
         res = await self.db_session.execute(query)
-        update_dis_id_row = res.fetchone()
-        if update_dis_id_row is not None:
-            return update_dis_id_row[0]
-
-    async def delete_discipline(self, dis_id) -> Discipline | None:
-        query = (
-            select(Discipline)
-            .where(Discipline.dis_id == dis_id)
-        )
-
-        res = await self.db_session.execute(query)
-        discipline = res.fetchone()
-        if discipline:
-            await self.db_session.delete(discipline)
-            await self.db_session.commit()
-            return discipline
+        update_dis_id: int | None = res.scalar_one_or_none()
+        if update_dis_id is not None:
+            return await self.get_discipline_by_id(update_dis_id)
